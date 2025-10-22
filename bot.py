176c743a49b03c04d3cf633262ea73c65a0d3462
Ethan <toga56784@gmail.com>
2025-10-22 18:18:48 +0100

    Implement panic command for alert notifications
    Added a panic command to send alerts to a specific channel.

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-import logging
 import os
-
-token = BOT_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,4 +13,17 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-bot.run(token)
+    try:
+        synced = await bot.tree.sync(guild=discord.Object(id=1428777931395829822))
+        print(f'Synced {len(synced)} command(s).')
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="panic", guild=discord.Object(id=1428777931395829822))
+async def panic(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Panic Alert Successful.', ephemeral=True)
+    alert_channel = bot.get_channel(1430294741567799567)
+
+    await alert_channel.send(f'# 🚨 Panic Alert 🚨\n Panic alert sent by {interaction.user.mention}.\n React ✅ if you are responding. <@&1430602297804722226>')
+
+bot.run(BOT_TOKEN)
