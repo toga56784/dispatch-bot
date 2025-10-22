78a358381640b48b5a5f6e412beb60cd6b2de64b
Ethan Hogan-Cloke <toga56784@gmail.com>
2025-10-22 17:04:49 +0100

    Made basic bot

--- bot.py
+++ bot.py
@@ -1,3 +1,18 @@
 import discord
+from discord.ext import commands
+import logging
+import os
 
-bot = discord.client()
+token = BOT_TOKEN
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+bot.run(token)
